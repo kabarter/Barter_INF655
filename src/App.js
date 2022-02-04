b8fe0b0249109ec1519d8b01021c1629424c68d2
kabarter <50552547+kabarter@users.noreply.github.com>
2022-02-04 17:42:18 -0500

    adding files first time

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myName = [
+    {
+    id: 1,
+    name: "Karen",
+  },
+  {
+    id: 2,
+    name: "Bruce",
+  },
+  {
+    id: 3,
+    name: "Piper",
+  },
+  {
+    id: 4,
+    name: "Gus",
+  },
+  {
+    id: 5,
+    name: "Maggie",
+  },
+  ];
+
+  const toDoList = [
+    {
+      id: 1,
+      title: "Clean Office"
+    },
+    {
+      id: 2,
+      title: "Run Errands"
+    },
+    {
+      id: 3,
+      title: "Walk Dog"
+    },
+    {
+      id: 4,
+      title: "Garden"
+    },
+    {
+      id: 5,
+      title: "Fix Car"
+    },
+    {
+      id: 6,
+      title: "Complete Assignments"
+    }
+  ];
+
+  const randomName = () => {
+    const int = Math.floor(Math.random() *5);
+    return myName[int].name;
+  }
+
+  const random = () => {
+    const int = Math.floor(Math.random() *6);
+    return toDoList[int].title;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Assignment 1</h1>
+       
+       <p>Hello {randomName()} you have a task {random()} to finish.
+       </p>  
+       
+       
       </header>
     </div>
   );
